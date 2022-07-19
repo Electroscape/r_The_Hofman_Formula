7613b745520de78104efbe37f222baa5a8dc0bfd
Seif elbouhy <seif.elbouhy@gmail.com>
2022-07-19 12:22:43 +0200

    add black screen
    background black image

--- MICROSCOPE/start.sh
+++ MICROSCOPE/start.sh
@@ -1,9 +1,11 @@
 export DISPLAY=:0
 xhost +
 
+sudo pkill fbi
 sudo pkill python
 sudo pkill vlc
 
 cd ~/MICROSCOPE/src
+sudo fbi -T 1 -noverbose -a ~/MICROSCOPE/img/hh/black_screen.jpg &
 
 python3 ~/MICROSCOPE/src/microscope.py -c hh
